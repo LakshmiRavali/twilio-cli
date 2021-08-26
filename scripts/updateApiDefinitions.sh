095dc8b79011e113db281c4286fdb959ff39e95a
lakshmiravali <lakshmiravali.rimmalapudi@gmail.com>
2021-08-26 18:59:11 +0530

    fix: Added all the changes

--- scripts/updateApiDefinitions.sh
+++ scripts/updateApiDefinitions.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
-echo "Copying api-definitions changelog"
 echo "Running update changelog script"
-changeLog=$(node scripts/update-api-definitions.js)
-changeLog="${changeLog//'%'/'%25'}"
-changeLog="${changeLog//$'\n'/'%0A'}"
-changeLog="${changeLog//$'\r'/'%0D'}"
-echo "CHANGE_LOG=$changeLog" >> $GITHUB_ENV
-rm -rf OAI_CHANGES.md
+node scripts/update-change-log.js "$1"
 echo "Git configurations"
 git config --global user.email "lakshmiravali.rimmalapudi@gmail.com"
 git config --global user.name "lakshmiravali"
-git add .
-commitMessage='fix: Updated api definitions changelog in CHANGES.md in cli'
-echo "Commit message:$commitMessage"
-git commit -m "$commitMessage"
-git push origin test_branch
+git add -A
+if [ -n "$(git status --porcelain)" ]; then
+  branch=$(git branch --show-current)
+  echo "Current branch: $branch"
+  echo "There are changes to commit.";
+  commitMessage=''
+  if [ "$2" == 0 ] || [ "$2" == 1 ]
+  then
+    commitMessage='feat: Updated api definitions changelog in CHANGES.md'
+  elif [ "$2" == 2 ]
+  then
+    commitMessage='fix: Updated api definitions changelog in CHANGES.md'
+  else
+    echo "Invalid versionType: $2";
+    exit
+  fi
+  echo "Commit message:$commitMessage"
+  git commit -m "$commitMessage"
+  git push origin "$branch"
+else
+  echo "No changes to commit";
+fi
